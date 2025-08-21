a7db34822fe8528820f1f73b1eaeb9aa93e13611
thor-x-me <abhishekverm2018@gmail.com>
2025-08-21 22:05:04 +0530

    table definitions of video to quiz added

---
+++ database/db_models.py
@@ -0,0 +1,92 @@
+from sqlalchemy import Column, Integer, String, create_engine, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from datetime import datetime
+
+engine = create_engine('sqlite:///../database/database.db', echo=True)
+Base = declarative_base()
+
+class VideoCache(Base):
+    """
+    This table stores video information.
+    """
+    __tablename__ = "video_cache"
+
+    video_id = Column(String, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    length = Column(Integer, nullable=False)
+    views = Column(Integer, nullable=False)
+    description = Column(String, nullable=False)
+    keywords = Column(String, nullable=False)
+    thumbnail_url = Column(String, nullable=False)
+
+class VideoToQuiz(Base):
+    """
+    This table all the quiz information, each quiz have an id,
+     and it can be used to get question from MCQsVideoToQuiz table.
+    """
+    __tablename__ = "video_to_quiz"
+
+    id = Column(Integer, primary_key=True)
+    created_by = Column(String, nullable=False)
+    date_created = Column(DateTime, nullable=False, default=datetime.now)
+    video_id = Column(String, nullable=False)
+    video_title = Column(String, nullable=False)
+    difficulty = Column(String, nullable=False)
+    no_of_questions = Column(Integer, nullable=False)
+    mcqs = relationship("MCQsVideoToQuiz", back_populates="quiz")
+
+class VideoToQuizQuota(Base):
+    """
+    This table store quota of each user for generating quiz (not perfect, must be updated properly)
+    """
+    __tablename__ = "video_to_quiz_quota"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String, nullable=False, unique=True)
+    quota_remaining = Column(Integer, nullable=False, default=50)
+    last_reset_date = Column(DateTime, default=datetime.now)
+
+class MCQsVideoToQuiz(Base):
+    """
+    This table stores multiple choice questions,
+     each question is connected to an id in VideoToQuiz table through foreign key.
+    """
+    __tablename__ = "mcqs_video_to_quiz"
+
+    question_id = Column(Integer, primary_key=True, autoincrement=True)
+    quiz_id = Column(Integer, ForeignKey("video_to_quiz.id"))
+
+    question = Column(String, nullable=False)
+    a = Column(String, nullable=False)
+    b = Column(String, nullable=False)
+    c = Column(String, nullable=False)
+    d = Column(String, nullable=False)
+    correct_answer = Column(String, nullable=False)
+    explanation = Column(String, nullable=False)
+
+    quiz = relationship("VideoToQuiz", back_populates="mcqs")
+
+class MCQsVideoToQuizResult(Base):
+    """
+    This table store result of a quiz attempt by any user.
+    """
+    __tablename__ = "mcqs_video_to_quiz_result"
+
+    quiz_id = Column(Integer, ForeignKey("video_to_quiz.id"), primary_key=True)
+    user_id = Column(String, nullable=False)
+    total_correct_attempt = Column(Integer, nullable=False)
+    total_wrong_attempt = Column(Integer, nullable=False)
+    not_attempted = Column(Integer, nullable=False)
+
+Base.metadata.create_all(engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
