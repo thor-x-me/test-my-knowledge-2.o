2f12a0bdae1970877e7a6e092169d8135d7d0903
Abhishek Verma <abhishekverm2018@gmail.com>
2026-02-10 00:11:08 +0530

    utility function to find audio file type

--- backend/utils.py
+++ backend/utils.py
@@ -1,4 +1,5 @@
 import os
+import filetype
 from dotenv import load_dotenv
 from fastapi import HTTPException, Request
 from clerk_backend_api import Clerk, AuthenticateRequestOptions
@@ -28,3 +29,10 @@ def authenticate_user_get_user_details(request):
 
     except Exception as e:
         raise HTTPException(status_code=401, detail="Invalid authentication")
+
+
+def get_audio_format(file_path: str) -> str | None:
+    kind = filetype.guess(file_path)
+    if kind:
+        return kind.extension  # e.g. "mp3", "wav", "ogg", "flac"
+    return None
