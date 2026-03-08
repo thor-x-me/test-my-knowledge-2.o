f6983b73396f5f366f15d8ac9356390f1a9d1bc8
Abhishek Verma <abhishekverm2018@gmail.com>
2026-03-09 01:38:31 +0530

    fixed auth method and implemented logging

--- backend/utils.py
+++ backend/utils.py
@@ -3,23 +3,27 @@ import filetype
 from dotenv import load_dotenv
 from fastapi import HTTPException, Request
 from clerk_backend_api import Clerk, AuthenticateRequestOptions
+import logging
 
 
 load_dotenv()
+logger = logging.getLogger(__name__)
 
 
 clerk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
 
 def authenticate_user_get_user_details(request):
-
+    auth_header = request.headers.get("authorization")
+    print("Authorization header:", auth_header)
     try:
         request_state = clerk.authenticate_request(
             request,
             AuthenticateRequestOptions(
-                authorized_parties=["http://localhost:3000"],   #use the frontend port address or source of request
+                # authorized_parties=["http://localhost:3000", "http://localhost:8000"],   #use the frontend port address or source of request
                 jwt_key=os.getenv("JWT_KEY")
             )
         )
+        print(request_state)
         if not request_state.is_signed_in:
             raise HTTPException(status_code=401, detail="Invalid token")
 
@@ -28,6 +32,7 @@ def authenticate_user_get_user_details(request):
         return {"user_id": user_id}
 
     except Exception as e:
+        logger.exception(f"Error logging in user")
         raise HTTPException(status_code=401, detail="Invalid authentication")
 
 
