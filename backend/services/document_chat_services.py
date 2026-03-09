9d47c9e99d7f39d561d1e48b41f61d81f8ce88eb
Abhishek Verma <abhishekverm2018@gmail.com>
2026-03-09 23:19:21 +0530

    fixed summarization

--- backend/services/document_chat_services.py
+++ backend/services/document_chat_services.py
@@ -4,7 +4,7 @@ from backend.services.gemini_services import GeminiService
 from google.genai.types import File
 from pydantic import BaseModel
 from dotenv import load_dotenv
-from backend.prompts.summary_generator_prompt import prompt
+from backend.prompts import summary_generator_prompt
 load_dotenv()
 
 
@@ -51,6 +51,6 @@ class DocumentChatService:
         return response
 
     def summarize_chat(self, chat_history):
-        summary = self.model.generate_response(prompt.prompt + chat_history)
+        summary = self.model.text_completion(summary_generator_prompt + chat_history)
         return summary
 
