224131803f658a753ecf8af158437c38af9be0b3
Abhishek Verma <abhishekverm2018@gmail.com>
2026-04-07 21:53:50 +0530

    removed old frontend file

--- frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-};
